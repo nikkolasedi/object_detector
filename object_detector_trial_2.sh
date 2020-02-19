7bcc8012dbbfa589a2576fa8a3d04132a5fc0c8d
Nikkolas <pujantoro@fzi.de>
2020-02-19 20:34:27 +0100

    New shell script for running all packages Use this

---
+++ object_detector_trial_2.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+tmux new-session -d -s real
+
+
+tmux set -g mouse on
+
+# NAME1
+tmux new-window
+tmux rename-window 'roscore'
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "" ENTER
+# NAME2
+tmux split-window -h
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "cd rosbag" ENTER
+tmux send-keys "rosbag play 3_no_hand.bag --loop" ENTER
+# NAME3
+tmux split-window -v
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rosrun object_detector pcl_filter" ENTER
+# NAME4
+tmux select-pane -L
+tmux split-window -v
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rosrun object_detector pcl_crop" ENTER
+
+# NAME5
+tmux new-window
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rosrun object_detector inter_marker" ENTER
+# NAME6
+tmux split-window -h
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rviz -d ~/.rviz/pcl_only.rviz" ENTER
+# NAME7
+tmux split-window -v
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rviz -d ~/.rviz/marker_only.rviz" ENTER
+# NAME8
+tmux select-pane -L
+tmux split-window -v
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rqt_graph" ENTER
+
+# NAME9
+tmux new-window
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "roslaunch ros_posenet posenet.launch" ENTER
+# NAME10
+tmux split-window -h
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "cd ~/robot_folders-master/checkout/object_detector/catkin_ws/src/ros_posenet/rosbag" ENTER
+tmux send-keys "rosbag play 4_hand_left.bag --loop" ENTER
+# NAME11
+tmux split-window -v
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rosrun ros_posenet listener.py" ENTER
+# NAME12
+tmux select-pane -L
+tmux split-window -v
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rosrun ros_posenet pose_marker" ENTER
+# NAME13
+tmux select-pane -R
+tmux split-window -v
+tmux send-keys "ce object_detector" ENTER
+tmux send-keys "rviz -d ~/.rviz/posenet_marker.rviz" ENTER
+
+
+## Start a new line on window 0
+tmux send-keys -t real ENTER
+
+tmux attach -t real
+
